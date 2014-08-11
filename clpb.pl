6ad432c6e199abef84f35db9a3c0db37847b0bec
Markus Triska <markus.triska@gmx.at>
2014-08-11 22:27:03 +0200

    generalise unification

--- clpb.pl
+++ clpb.pl
@@ -209,7 +209,7 @@ sat(Sat0) :-
         parse_sat(Sat0, Sat),
         sat_bdd(Sat, BDD),
         sat_roots(Sat, Roots),
-        foldl(root_and, Roots, Sat-BDD, And-BDD1),
+        foldl(root_and, Roots, Sat0-BDD, And-BDD1),
         maplist(del_bdd, Roots),
         maplist(=(Root), Roots),
         put_attr(Root, bdd, And-BDD1),
@@ -435,7 +435,7 @@ attr_unify_hook(index_root(I,Root), Other) :-
             Sat = Sat0*OtherSat,
             sat_roots(Sat, Roots),
             maplist(root_rebuild_bdd, Roots),
-            foldl(root_and, Roots, i(1)-1, And-BDD1),
+            foldl(root_and, Roots, 1-1, And-BDD1),
             maplist(del_bdd, Roots),
             maplist(=(NewRoot), Roots),
             put_attr(NewRoot, bdd, And-BDD1),
@@ -443,9 +443,12 @@ attr_unify_hook(index_root(I,Root), Other) :-
         ).
 
 root_rebuild_bdd(Root) :-
-        get_attr(Root, bdd, F0-_),
-        sat_bdd(F0, BDD),
-        put_attr(Root, bdd, F0-BDD).
+        (   get_attr(Root, bdd, F0-_) ->
+            parse_sat(F0, Sat),
+            sat_bdd(Sat, BDD),
+            put_attr(Root, bdd, F0-BDD)
+        ;   true
+        ).
 
 is_bdd(BDD) :-
         catch((phrase(bdd_ite(BDD), ITEs0),
