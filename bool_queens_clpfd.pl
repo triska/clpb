b77aae7a3bcc26a770cbd085780a2702c8756ac2
Markus Triska <markus.triska@gmx.at>
2014-08-30 17:42:10 +0200

    optionally, build a large conjunction

--- bool_queens_clpfd.pl
+++ bool_queens_clpfd.pl
@@ -26,8 +26,6 @@ run :-
         statistics(cputime, T0),
         n_queens(N, Qs),
         append(Qs, Vs),
-        % foldl(or, Vs, 1, Or),
-        % Or,
         findall(., label(Vs), Sols),
         length(Sols, C),
         statistics(cputime, T1),
@@ -38,63 +36,77 @@ run :-
 n_queens(N, Qs) :-
         length(Qs, N),
         maplist(length_(N), Qs),
+        maplist(one_in_row, Qs),
         maplist(row, Qs),
         transpose(Qs, TQs),
+        maplist(one_in_row, TQs),
         maplist(row, TQs),
-        diagonals(Qs, 1, 1, N).
+        phrase(diagonals(Qs, 1, 1, N), Ands),
+        foldl(and, Ands, 1#/\1, And),
+        And.
+
+one_in_row(Row) :-
+        foldl(or, Row, 0#\/0, Or),
+        Or.
 
 row(Row) :-
-        foldl(or, Row, 0, Or),
-        Or,
-        not_same_row(Row).
+        phrase(not_same_row(Row), Ands),
+        foldl(and, Ands, 1#/\1, And),
+        And.
 
-not_same_row([]).
-not_same_row([Q|Qs]) :-
+and(A, B, B #/\ A).
+
+not_same_row([]) --> [].
+not_same_row([Q|Qs]) -->
         not_same_row_(Qs, Q),
         not_same_row(Qs).
 
 or(A, B, B #\/ A).
 
-not_same_row_([], _).
-not_same_row_([L|Ls], Q) :-
-        Q #==> #\ L,
+not_same_row_([], _) --> [].
+not_same_row_([L|Ls], Q) -->
+        not_same(Q, L),
         not_same_row_(Ls, Q).
+
+
+not_same(A, B) --> [#\ ( A #/\ B)].
+%not_same(A, B) --> { #\ (A #/\ B) }.
 
 length_(L, Ls) :- length(Ls, L).
 
-diagonals(Qs, Row, Col, N) :-
-        (   Row > N -> true
-        ;   Col > N ->
-            Row1 is Row + 1,
+diagonals(Qs, Row, Col, N) -->
+        (   { Row > N } -> []
+        ;   { Col > N } ->
+            { Row1 is Row + 1 },
             diagonals(Qs, Row1, 1, N)
-        ;   queen_at(Qs, Row, Col, Q),
-            DRow is Row + 1,
-            DCol is Col + 1,
+        ;   { queen_at(Qs, Row, Col, Q),
+              DRow is Row + 1,
+              DCol is Col + 1 },
             diagonal_down(Qs, DRow, DCol, N, Q),
-            URow is Row - 1,
-            UCol is Col + 1,
+            { URow is Row - 1,
+              UCol is Col + 1 },
             diagonal_up(Qs, URow, UCol, N, Q),
-            Col1 is Col + 1,
+            { Col1 is Col + 1 },
             diagonals(Qs, Row, Col1, N)
         ).
 
-diagonal_down(Qs, Row, Col, N,Q) :-
-        (   Row > N -> true
-        ;   Col > N -> true
-        ;   queen_at(Qs, Row, Col, Q0),
-            Q #==> #\ Q0,
-            Row1 is Row + 1,
-            Col1 is Col + 1,
+diagonal_down(Qs, Row, Col, N,Q) -->
+        (   { Row > N } -> []
+        ;   { Col > N } -> []
+        ;   { queen_at(Qs, Row, Col, Q0) },
+            not_same(Q, Q0),
+            { Row1 is Row + 1,
+              Col1 is Col + 1 },
             diagonal_down(Qs, Row1, Col1, N, Q)
         ).
 
-diagonal_up(Qs, Row, Col, N, Q) :-
-        (   Row < 1 -> true
-        ;   Col > N -> true
-        ;   queen_at(Qs, Row, Col, Q0),
-            Q #==> #\ Q0,
-            Row1 is Row - 1,
-            Col1 is Col + 1,
+diagonal_up(Qs, Row, Col, N, Q) -->
+        (   { Row < 1 } -> []
+        ;   { Col > N } -> []
+        ;   { queen_at(Qs, Row, Col, Q0) },
+            not_same(Q, Q0),
+            { Row1 is Row - 1,
+              Col1 is Col + 1 },
             diagonal_up(Qs, Row1, Col1, N, Q)
         ).
 
