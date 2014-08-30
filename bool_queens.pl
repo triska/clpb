4253c77875712346e8f8c4d7cceb8de69edd0b63
Markus Triska <markus.triska@gmx.at>
2014-08-30 17:26:33 +0200

    optionally build a large conjunction instead of multiple sat/1 calls
    These two formulations should have roughly the same performance.

--- bool_queens.pl
+++ bool_queens.pl
@@ -4,9 +4,12 @@
 %?- run.
 %@ |queens(0)| = 1           after 0.00s
 %@ |queens(1)| = 1           after 0.00s
-%@ |queens(4)| = 2           after 0.17s
-%@ |queens(5)| = 10          after 1.11s
-%@ |queens(6)| = 4           after 5.70s
+%@ |queens(4)| = 2           after 0.08s
+%@ |queens(5)| = 10          after 0.53s
+%@ |queens(6)| = 4           after 2.81s
+%@ |queens(7)| = 40          after 14.32s
+%@ |queens(8)| = 92          after 73.07s
+
 
 %?- n_queens(4, Qs), append(Qs, Vs), labeling(Vs), maplist(writeln, Qs).
 %@ [0,0,1,0]
@@ -19,6 +22,11 @@
 run :-
         length(_, N),
         statistics(cputime, T0),
+        length(Qs, N),
+        maplist(length_(N), Qs),
+        append(Qs, Vs),
+        % foldl(or, Vs, 1, Or),
+        % sat(Or), % force variables into same BDD
         n_queens(N, Qs),
         append(Qs, Vs),
         foldl(or, Vs, 1, Or),
@@ -31,64 +39,77 @@ run :-
 n_queens(N, Qs) :-
         length(Qs, N),
         maplist(length_(N), Qs),
+        maplist(one_in_row, Qs),
         maplist(row, Qs),
         transpose(Qs, TQs),
+        maplist(one_in_row, TQs),
         maplist(row, TQs),
-        diagonals(Qs, 1, 1, N).
+        phrase(diagonals(Qs, 1, 1, N), Ands),
+        foldl(and, Ands, 1, And),
+        sat(And).
+
+one_in_row(Row) :-
+        foldl(or, Row, 0, Or),
+        sat(Or).
 
 row(Row) :-
-        foldl(or, Row, 0, Or),
-        sat(Or),
-        not_same_row(Row).
+        phrase(not_same_row(Row), Ands),
+        foldl(and, Ands, 1, And),
+        sat(And).
 
-not_same_row([]).
-not_same_row([Q|Qs]) :-
+
+not_same_row([]) --> [].
+not_same_row([Q|Qs]) -->
         not_same_row_(Qs, Q),
         not_same_row(Qs).
 
 or(A, B, B + A).
 
-not_same_row_([], _).
-not_same_row_([L|Ls], Q) :-
-        % Q ==> ~L
-        sat(~Q + ~L),
+and(A, B, B * A).
+
+not_same_row_([], _) --> [].
+not_same_row_([L|Ls], Q) -->
+        not_same(Q, L),
         not_same_row_(Ls, Q).
+
+%not_same(A, B) --> [~(A * B)].
+not_same(A, B) --> { sat(~(A * B)) }.
 
 length_(L, Ls) :- length(Ls, L).
 
-diagonals(Qs, Row, Col, N) :-
-        (   Row > N -> true
-        ;   Col > N ->
-            Row1 is Row + 1,
+diagonals(Qs, Row, Col, N) -->
+        (   { Row > N } -> []
+        ;   { Col > N } ->
+            { Row1 is Row + 1 },
             diagonals(Qs, Row1, 1, N)
-        ;   queen_at(Qs, Row, Col, Q),
-            DRow is Row + 1,
-            DCol is Col + 1,
+        ;   { queen_at(Qs, Row, Col, Q),
+              DRow is Row + 1,
+              DCol is Col + 1 },
             diagonal_down(Qs, DRow, DCol, N, Q),
-            URow is Row - 1,
-            UCol is Col + 1,
+            { URow is Row - 1,
+              UCol is Col + 1 },
             diagonal_up(Qs, URow, UCol, N, Q),
-            Col1 is Col + 1,
+            { Col1 is Col + 1 },
             diagonals(Qs, Row, Col1, N)
         ).
 
-diagonal_down(Qs, Row, Col, N,Q) :-
-        (   Row > N -> true
-        ;   Col > N -> true
-        ;   queen_at(Qs, Row, Col, Q0),
-            sat(~Q + ~Q0),
-            Row1 is Row + 1,
-            Col1 is Col + 1,
+diagonal_down(Qs, Row, Col, N,Q) -->
+        (   { Row > N } -> []
+        ;   { Col > N } -> []
+        ;   { queen_at(Qs, Row, Col, Q0) },
+            not_same(Q, Q0),
+            { Row1 is Row + 1,
+              Col1 is Col + 1 },
             diagonal_down(Qs, Row1, Col1, N, Q)
         ).
 
-diagonal_up(Qs, Row, Col, N, Q) :-
-        (   Row < 1 -> true
-        ;   Col > N -> true
-        ;   queen_at(Qs, Row, Col, Q0),
-            sat(~Q + ~Q0),
-            Row1 is Row - 1,
-            Col1 is Col + 1,
+diagonal_up(Qs, Row, Col, N, Q) -->
+        (   { Row < 1 } -> []
+        ;   { Col > N } -> []
+        ;   { queen_at(Qs, Row, Col, Q0) },
+            not_same(Q, Q0),
+            { Row1 is Row - 1,
+              Col1 is Col + 1 },
             diagonal_up(Qs, Row1, Col1, N, Q)
         ).
 
