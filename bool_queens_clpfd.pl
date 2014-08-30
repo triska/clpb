73fdde7a19fd18fd037a4f533982708225036a28
Markus Triska <markus.triska@gmx.at>
2014-08-30 17:29:11 +0200

    express with CLP(FD) for comparison

--- bool_queens_clpfd.pl
+++ bool_queens_clpfd.pl
@@ -1,14 +1,19 @@
-:- use_module(clpb).
 :- use_module(library(clpfd)).
 
 %?- run.
 %@ |queens(0)| = 1           after 0.00s
 %@ |queens(1)| = 1           after 0.00s
-%@ |queens(4)| = 2           after 0.17s
-%@ |queens(5)| = 10          after 1.11s
-%@ |queens(6)| = 4           after 5.70s
+%@ |queens(2)| = 0           after 0.00s
+%@ |queens(3)| = 0           after 0.01s
+%@ |queens(4)| = 2           after 0.02s
+%@ |queens(5)| = 10          after 0.05s
+%@ |queens(6)| = 4           after 0.11s
+%@ |queens(7)| = 40          after 0.26s
+%@ |queens(8)| = 92          after 0.72s
+%@ |queens(9)| = 352         after 2.52s
+%@ |queens(10)| = 724        after 9.74s
 
-%?- n_queens(4, Qs), append(Qs, Vs), labeling(Vs), maplist(writeln, Qs).
+%?- n_queens(4, Qs), append(Qs, Vs), label(Vs), maplist(writeln, Qs).
 %@ [0,0,1,0]
 %@ [1,0,0,0]
 %@ [0,0,0,1]
@@ -21,8 +26,10 @@ run :-
         statistics(cputime, T0),
         n_queens(N, Qs),
         append(Qs, Vs),
-        foldl(or, Vs, 1, Or),
-        sat_count(Or, C),
+        % foldl(or, Vs, 1, Or),
+        % Or,
+        findall(., label(Vs), Sols),
+        length(Sols, C),
         statistics(cputime, T1),
         Time is T1 - T0,
         format("|queens(~w)| = ~w  ~25| after ~2fs\n", [N,C,Time]),
@@ -38,7 +45,7 @@ n_queens(N, Qs) :-
 
 row(Row) :-
         foldl(or, Row, 0, Or),
-        sat(Or),
+        Or,
         not_same_row(Row).
 
 not_same_row([]).
@@ -46,12 +53,11 @@ not_same_row([Q|Qs]) :-
         not_same_row_(Qs, Q),
         not_same_row(Qs).
 
-or(A, B, B + A).
+or(A, B, B #\/ A).
 
 not_same_row_([], _).
 not_same_row_([L|Ls], Q) :-
-        % Q ==> ~L
-        sat(~Q + ~L),
+        Q #==> #\ L,
         not_same_row_(Ls, Q).
 
 length_(L, Ls) :- length(Ls, L).
@@ -76,7 +82,7 @@ diagonal_down(Qs, Row, Col, N,Q) :-
         (   Row > N -> true
         ;   Col > N -> true
         ;   queen_at(Qs, Row, Col, Q0),
-            sat(~Q + ~Q0),
+            Q #==> #\ Q0,
             Row1 is Row + 1,
             Col1 is Col + 1,
             diagonal_down(Qs, Row1, Col1, N, Q)
@@ -86,7 +92,7 @@ diagonal_up(Qs, Row, Col, N, Q) :-
         (   Row < 1 -> true
         ;   Col > N -> true
         ;   queen_at(Qs, Row, Col, Q0),
-            sat(~Q + ~Q0),
+            Q #==> #\ Q0,
             Row1 is Row - 1,
             Col1 is Col + 1,
             diagonal_up(Qs, Row1, Col1, N, Q)
