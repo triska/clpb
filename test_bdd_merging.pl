ee2305c5b7f22856f24fb1a1a53268941c9c0139
Markus Triska <markus.triska@gmx.at>
2014-08-31 10:00:18 +0200

    new test for bdd_and/3, merging multiple BDDs
    This test finds the now corrected mistake of placing rebuild_hashes/1 in
    bdd_and/3, clearing local tables even though the nodes still occur in
    other BDDs. This test is faster than the one shown in the git commit.

---
+++ test_bdd_merging.pl
@@ -0,0 +1,29 @@
+:- use_module(clpb).
+
+
+sat(X, Y, X=:=Y).
+sat(X, Y, X#Y).
+sat(X, Y, X+Y).
+sat(X, Y, X*Y).
+
+
+vs_formula([V|Vs], F) :- vs_formula_(Vs, V, F).
+
+vs_formula_([], F, F).
+vs_formula_([V|Vs], F0, F) :-
+        sat(V, F0, Sat),
+        vs_formula_(Vs, Sat, F).
+
+run :-
+        sat(A, B, Sat1),
+        sat(C, D, Sat2),
+        sat(E, F, Sat3),
+        sat(Sat1),
+        sat(Sat2),
+        sat(Sat3),
+        vs_formula([A,B,C,D,E,F], Formula),
+        sat(Formula),
+        false.
+
+
+%?- run.
