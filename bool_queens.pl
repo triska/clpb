bd49625de97090851d02c9fb5513177145abbafb
Markus Triska <markus.triska@gmx.at>
2015-08-09 11:40:45 +0200

    make the conjunction explicitly available

--- bool_queens.pl
+++ bool_queens.pl
@@ -6,29 +6,26 @@
 %@ |queens(1)| = 1           after 0.00s
 %@ |queens(2)| = 0           after 0.00s
 %@ |queens(3)| = 0           after 0.01s
-%@ |queens(4)| = 2           after 0.04s
-%@ |queens(5)| = 10          after 0.25s
-%@ |queens(6)| = 4           after 1.02s
-%@ |queens(7)| = 40          after 5.90s
-%@ |queens(8)| = 92          after 30.51s
+%@ |queens(4)| = 2           after 0.12s
+%@ |queens(5)| = 10          after 0.88s
+%@ |queens(6)| = 4           after 6.69s
+%@ |queens(7)| = 40          after 39.58s
+%@ etc.
 
 
-%?- n_queens(4, Qs), append(Qs, Vs), labeling(Vs), maplist(writeln, Qs).
+%?- n_queens(4, Qs, _, Sat), sat(Sat), append(Qs, Vs), labeling(Vs), maplist(writeln, Qs).
 %@ [0,0,1,0]
 %@ [1,0,0,0]
 %@ [0,0,0,1]
 %@ [0,1,0,0]
-%@ Qs = [[0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0]],
-%@ Vs = [0, 0, 1, 0, 1, 0, 0, 0, 0|...] .
+%@ etc.
 
 run :-
         length(_, N),
         statistics(cputime, T0),
-        length(Qs, N),
-        maplist(length_(N), Qs),
+        n_queens(N, Qs, _, Sat),
         append(Qs, Vs),
-        % sat(+[1|Vs]), % force variables into same BDD
-        (   n_queens(N, Qs) ->
+        (   sat(Sat) ->
             sat_count(+[1|Vs], C)
         ;   C = 0
         ),
@@ -37,18 +34,28 @@ run :-
         format("|queens(~w)| = ~w  ~25| after ~2fs\n", [N,C,Time]),
         false.
 
-n_queens(N, Qs) :-
+n_queens(N, Qs, Ands, *(Ands)) :-
         length(Qs, N),
         maplist(length_(N), Qs),
-        maplist(row, Qs),
         transpose(Qs, TQs),
-        maplist(row, TQs),
-        phrase(diagonals(Qs, 1, 1, N), Ds),
-        maplist(at_most_one, Ds).
+        phrase((rows(Qs),rows(TQs),
+                diagonals(Qs, 1, 1, N)), Ands).
 
-at_most_one(Ls) :- sat(card([0,1], Ls)).
+rows([]) --> [].
+rows([Row|Rows]) -->
+        [+Row],
+        not_same_row(Row),
+        rows(Rows).
 
-row(Row) :- sat(card([1], Row)).
+not_same_row([]) --> [].
+not_same_row([Q|Qs]) -->
+        not_same_row_(Qs, Q),
+        not_same_row(Qs).
+
+not_same_row_([], _) --> [].
+not_same_row_([L|Ls], Q) -->
+        [~Q + ~L],
+        not_same_row_(Ls, Q).
 
 length_(L, Ls) :- length(Ls, L).
 
@@ -57,34 +64,39 @@ diagonals(Qs, Row, Col, N) -->
         ;   { Col > N } ->
             { Row1 #= Row + 1 },
             diagonals(Qs, Row1, 1, N)
-        ;   { phrase(diagonal_down(Qs, Row, Col, N), Ds) },
-            [Ds],
-            { phrase(diagonal_up(Qs, Row, Col, N), Us) },
-            [Us],
+        ;   { queen_at(Qs, Row, Col, Q),
+              DRow #= Row + 1,
+              DCol #= Col + 1 },
+            diagonal_down(Qs, DRow, DCol, N, Q),
+            { URow #= Row - 1,
+              UCol #= Col + 1 },
+            diagonal_up(Qs, URow, UCol, N, Q),
             { Col1 #= Col + 1 },
             diagonals(Qs, Row, Col1, N)
         ).
 
-diagonal_down(Qs, Row, Col, N) -->
+diagonal_down(Qs, Row, Col, N,Q) -->
         (   { Row > N } -> []
         ;   { Col > N } -> []
-        ;   queen_at(Qs, Row, Col),
+        ;   { queen_at(Qs, Row, Col, Q0) },
+            [~Q + ~Q0],
             { Row1 #= Row + 1,
               Col1 #= Col + 1 },
-            diagonal_down(Qs, Row1, Col1, N)
+            diagonal_down(Qs, Row1, Col1, N, Q)
         ).
 
-diagonal_up(Qs, Row, Col, N) -->
+diagonal_up(Qs, Row, Col, N, Q) -->
         (   { Row < 1 } -> []
         ;   { Col > N } -> []
-        ;   queen_at(Qs, Row, Col),
+        ;   { queen_at(Qs, Row, Col, Q0) },
+            [~Q + ~Q0],
             { Row1 #= Row - 1,
               Col1 #= Col + 1 },
-            diagonal_up(Qs, Row1, Col1, N)
+            diagonal_up(Qs, Row1, Col1, N, Q)
         ).
 
 
-queen_at(Qs, NRow, NCol) -->
-        { nth1(NRow, Qs, Row),
-          nth1(NCol, Row, Q) },
-        [Q].
+
+queen_at(Qs, NRow, NCol, Q) :-
+        nth1(NRow, Qs, Row),
+        nth1(NCol, Row, Q).
